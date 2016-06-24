2a50c701e74d82571eb42f47362a13bcb20f49e5
James Casey <james@chef.io>
2016-06-24 11:40:55 -0700

    add in source url and description to plan.sh

--- prometheus/plan.sh
+++ prometheus/plan.sh
@@ -3,6 +3,14 @@ pkg_origin=jamesc
 pkg_version=0.20.0
 pkg_maintainer="James Casey <jamesc.000@gmail.com>"
 pkg_license=('apache')
+pkg_upstream_url="https://github.com/jamesc/habitat-plans/"
+pkg_description=$(cat << EOF
+  # Prometheus
+  This is the habitat package for Prometheus, the open source monitoring solution.
+
+  See [https://prometheus.io](https://prometheus.io) for more details.
+EOF
+)
 
 pkg_source=https://github.com/prometheus/${pkg_name}/releases/download/${pkg_version}/${pkg_name}-${pkg_version}.linux-amd64.tar.gz
 pkg_shasum=660ba94efb1f4cff4934dc55bfe1b920f9bc4630bf731de4a67599b048c42c5c
